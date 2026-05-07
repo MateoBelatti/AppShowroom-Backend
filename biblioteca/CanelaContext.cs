6b75c5693d062b9c4bd482f5e3fe2fb6810e1836
BelattiMateo <mateobelatti7@gmail.com>
2026-05-07 14:14:13 -0300

    feat: context DB y primera migracion

---
+++ biblioteca/CanelaContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace biblioteca
+{
+    public class CanelaContext : DbContext
+    {
+        public CanelaContext(DbContextOptions<CanelaContext> options) : base(options) { }
+        public DbSet<clases.Vela> Velas { get; set; }
+        public DbSet<clases.Categoria> Categorias { get; set; }
+        public DbSet<clases.Usuario> Usuarios { get; set; }
+        public DbSet<clases.Producto> Productos { get; set; }
+        public DbSet<clases.Carrito> Carritos { get; set; }
+        public DbSet<clases.DetalleCarrito> DetallesCarrito { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Configuración de clase abstracta
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<clases.Producto>().UseTptMappingStrategy();
+
+        }
+    }
+}
