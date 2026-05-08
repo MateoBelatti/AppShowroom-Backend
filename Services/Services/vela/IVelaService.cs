98ef01fdd62f171d6193f84bdba58d528f3a044c
BelattiMateo <mateobelatti7@gmail.com>
2026-05-07 22:19:24 -0300

    feat: implementacion de inteface y service sin desarrollar de vela

---
+++ Services/Services/vela/IVelaService.cs
@@ -0,0 +1,16 @@
+﻿using biblioteca.clases;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services.Services.vela
+{
+    public interface IVelaService
+    {
+        public Vela[] findAll();
+        public Vela findById(int id);
+        public Vela create(Vela vela);
+        public Vela update(Vela vela);
+        public void delete(int idVela);
+    }
+}
---
+++ Services/Services/vela/VelaService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Services.Services.vela
+{
+    public class VelaService : IVelaService
+    {
+    }
+}
